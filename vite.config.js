3b74afbef84a3494ac16a1032abe735f4d6a6dd5
Krins Sutariya <krinspatel583@gmail.com>
2026-09-18 20:02:38 +0530

    Fixed Vercel blank screen issue

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/kabbadi/', // <-- Yeh check kar lein
+  base: '/', // <-- Isko '/kabbadi/' se hatakar '/' kar dein
 })
