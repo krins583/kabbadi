eb6361e85bc6ce4a2ec6fac1dc2a5091a60414d2
Krins Sutariya <krinspatel583@gmail.com>
2026-09-18 20:00:45 +0530

    first commit1

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// BrowserRouter ki jagah HashRouter import karein
+import { HashRouter, Routes, Route } from 'react-router-dom' 
 import './index.css'
 
-// Aapke dono components import ho rahe hain
 import App from './App.jsx'
 import TvScreen from './TvScreen.jsx' 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    {/* Yahan HashRouter lagayein */}
+    <HashRouter>
       <Routes>
-        {/* Mobile/Admin Controller */}
         <Route path="/" element={<App />} />
-        
-        {/* Public TV Screen */}
         <Route path="/tv" element={<TvScreen />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/kabbadi/', 
+  base: '/kabbadi/', // <-- Yeh check kar lein
 })
